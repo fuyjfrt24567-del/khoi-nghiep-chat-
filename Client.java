55f93a72dd4d64b4e0bbce10ca831045563a5a3f
Hung <fuyjfrt24567@gmail.com>
2026-03-15 14:59:42 +0700

    update chat code

--- Client.java
+++ Client.java
@@ -1,8 +1,65 @@
-import java.net.ServerSocket;
-import java.net.Socket;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+
 public class Client {
+
+    JFrame frame;
+    JTextArea chatArea;
+    JTextField chatField;
+
+    BufferedReader in;
+    PrintWriter out;
+
+    public Client() throws Exception {
+
+        Socket socket = new Socket("localhost",9999);
+
+        in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        out = new PrintWriter(socket.getOutputStream(),true);
+
+        frame = new JFrame("Chat Client");
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+
+        chatField = new JTextField();
+
+        frame.setLayout(new BorderLayout());
+
+        frame.add(new JScrollPane(chatArea),BorderLayout.CENTER);
+        frame.add(chatField,BorderLayout.SOUTH);
+
+        frame.setSize(400,400);
+        frame.setVisible(true);
+
+        chatField.addActionListener(e -> {
+
+            String msg = chatField.getText();
+
+            chatArea.append("Me: " + msg + "\n");
+
+            out.println(msg);
+
+            chatField.setText("");
+
+        });
+
+        while(true){
+
+            String msg = in.readLine();
+
+            chatArea.append("Server: " + msg + "\n");
+
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localhost", 9999);
-        System.out.println("Connected to server");
+
+        new Client();
+
     }
 }
--- Server.java
+++ Server.java
@@ -1,14 +1,68 @@
-import java.net.ServerSocket;
-import java.net.Socket;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+
 public class Server {
-    public static void main(String[] args) throws Exception {
+
+    JFrame frame;
+    JTextArea chatArea;
+    JTextField chatField;
+
+    BufferedReader in;
+    PrintWriter out;
+
+    public Server() throws Exception {
+
         ServerSocket serverSocket = new ServerSocket(9999);
         System.out.println("Server started...");
 
-        while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected");
+        Socket socket = serverSocket.accept();
+
+        in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        frame = new JFrame("Chat Server");
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+
+        chatField = new JTextField();
+
+        frame.setLayout(new BorderLayout());
+
+        frame.add(new JScrollPane(chatArea), BorderLayout.CENTER);
+        frame.add(chatField, BorderLayout.SOUTH);
+
+        frame.setSize(400,400);
+        frame.setVisible(true);
+
+        chatField.addActionListener(e -> {
+
+            String msg = chatField.getText();
+
+            chatArea.append("Server: " + msg + "\n");
+
+            out.println(msg);
+
+            chatField.setText("");
+
+        });
+
+        while(true){
+
+            String msg = in.readLine();
+
+            chatArea.append("Client: " + msg + "\n");
+
         }
     }
-}
 
+    public static void main(String[] args) throws Exception {
+
+        new Server();
+
+    }
+}
